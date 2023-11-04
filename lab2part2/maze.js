c6ccb741f6a9ce5a3c11606d7a2231bf5ec17863
abdouch45 <abdelmoudjib.chihoub@univ-constantine2.dz>
2023-11-05 00:14:57 +0100

    counter and timer added

--- lab2part2/maze.js
+++ lab2part2/maze.js
@@ -2,6 +2,11 @@
 const start=document.getElementById("start");
 const end=document.getElementById("end");
 const h2=document.getElementById("status");
+const timer=document.getElementById('timer');
+const counter=document.getElementById("counter");
+let losses=0;
+let wins=0;
+let  timerId;
 let gameGoing=1;
 const boundaries = document.getElementsByClassName("boundary");
 const rect = start.getBoundingClientRect();
@@ -14,8 +19,11 @@ function checkCheating(event) {
     for(var i = 0; i < boundaries.length; i++){
       boundaries[i].style.backgroundColor = "red";
       gameGoing=3;
- h2.innerText="dont cheat again:)";
    }
+   clearInterval(timerId);
+   h2.innerText="dont cheat again:)";
+   losses++;
+   counter.innerText=`Wins:${wins} losses:${losses}`;
   }
 }
 
@@ -23,11 +31,21 @@ document.addEventListener("mousemove",(event)=>checkCheating(event));
 
 function init(){
   gameGoing=2;
+  timer.innerText = "60";
   for(var i = 0; i < boundaries.length; i++){
     boundaries[i].style.backgroundColor = "#eeeeee";
  }
  h2.innerText="game on going";
+ const timerInterval = setInterval(() => {
+  timer.innerText -= 1;
 
+  if (timer.innerText === "0") {
+    clearInterval(timerInterval);
+    touchWall();
+  }
+}, 1000);
+
+ timerId = timerInterval;
 }
 
 function touchWall(){
@@ -36,6 +54,9 @@ function touchWall(){
     boundaries[i].style.backgroundColor = "red";
  }
  gameGoing=3;
+ losses++;
+ counter.innerText=`Wins:${wins} losses:${losses}`;
+ clearInterval(timerId);
  h2.innerText="you lost to play again click on S";}
 }
 start.addEventListener("click",init);
@@ -43,6 +64,16 @@ start.addEventListener("click",init);
 start.addEventListener("mouseover", () => {
     if(gameGoing===1){
         gameGoing=2;
+        const timerInterval = setInterval(() => {
+          timer.innerText -= 1;
+        
+          if (timer.innerText === "0") {
+            clearInterval(timerInterval);
+            touchWall();
+          }
+        }, 1000);
+        
+         timerId = timerInterval;
         h2.innerText="game on going";
         for (let i = 0; i < boundaries.length; i++) {
           const boundary = boundaries[i];
@@ -54,6 +85,9 @@ end.addEventListener("mouseover",()=>{
   if(gameGoing===2){
     gameGoing=3;
     h2.innerText="you Won to play again click on S";
+    wins++;
+   counter.innerText=`Wins:${wins} losses:${losses}`;
+   clearInterval(timerId);
   }
 })
 
